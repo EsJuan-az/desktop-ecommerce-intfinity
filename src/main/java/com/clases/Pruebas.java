6e2eda3150ab6409465293973da41f8cc0173ec2
SATVanegas <124936176+SATVanegas@users.noreply.github.com>
2023-12-27 19:11:50 -0500

    Version santi
    CX

---
+++ src/main/java/com/clases/Pruebas.java
@@ -0,0 +1,39 @@
+package com.clases;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Pruebas extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Carga el archivo FXML
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/controllers/Principal.fxml")));
+// Asegúrate de que la ruta al archivo FXML sea correcta.
+
+            // Crea la escena con la raíz cargada desde el FXML
+            Scene scene = new Scene(root);
+
+            // Establece la escena en el escenario principal
+            primaryStage.setScene(scene);
+
+            // Configura el título de la ventana si lo deseas
+            primaryStage.setTitle("Infinity Enterprise");
+
+            // Muestra la ventana
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Lanza la aplicación JavaFX
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,12 +13,13 @@ module com.intfinty.appintfinity {
     requires okhttp3;
     requires org.json;
 
-    // Abre los paquetes al módulo javafx.fxml y javafx.base
     opens com.controllers to javafx.fxml;
-    opens com.clases to javafx.base, javafx.fxml; // Asegúrate de que este es el paquete correcto de tu clase Provider
-    opens com.services to javafx.fxml, javafx.base; // Abre si es necesario para reflexión
-
     exports com.controllers;
     exports com.services;
-    // No es necesario exportar com.clases si solo se utiliza dentro del mismo módulo
+    opens com.services to javafx.fxml;
+    opens com.clases to javafx.base;
+
+    // Exporta el paquete com.clases al módulo javafx.graphics
+    exports com.clases to javafx.graphics;
 }
+
